6ca4c34bad7c1f195cbf7bc677b36c4d94ad32c1
alex-belto <58671690+alex-belto@users.noreply.github.com>
2020-11-13 18:13:15 +0200

    changed layout

--- layout.php
+++ layout.php
@@ -8,14 +8,25 @@
     <body>
         <header>
             <ul>
-                <li><a href = "index.php">Some Store</a></li>
+                <li><a href = "?index">Some Store</a></li>
                 <li><a href = "?products">Каталог Товаров</a></li>
                 <li><a href = "?contacts">Контакты</a></li>
-                <li><a href = "signOut.php">Выйти</a></li>
+                <?php
+                    if(isset($_SESSION['outh'])){?>
+                        <li><a href = "?logout">Выйти</a></li>
+                 <?php   }?>
+                 <?php
+                    if(!isset($_SESSION['outh'])){?>
+                        <li><a href = "?login">Войти</a></li>
+                        <li><a href = "?registration">Зарегистрироваться</a></li>
+                 <?php   }?>
+                    
             </ul>
         </header>
         <main>
-
+            <?= $formContent ?>
+            <?= $content ?>
+            <?php if(isset($_SESSION['message'])) echo $_SESSION['message']; unset($_SESSION['message']); ?>
         </main>
         <footer>
         </footer>
